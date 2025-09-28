70f21f3a059537262a494a8001e13b0100fd4f22
Max Parry <66670928+Buggem@users.noreply.github.com>
2025-09-28 13:03:24 +1000

    Update pong.js

--- pong.js
+++ pong.js
@@ -1,9 +1,26 @@
+/*
+ * ATARI-Style Pong
+ * Copyright (C) 2025  Buggem
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.en.html.
+ */
 // also check out https://openprocessing.org/user/329506 :)
 
 // config
 // --> dev
 let thumb = false;
-let fakeTs = true;
+let fakeTs = false; // ensure to disable fakeTs in production versions
 // --> score
 let scoreScale = 10;
 let scoreGap   = 0; // defined in setup
