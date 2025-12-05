6c8ee9497d8e8376fe662032178b192e17608230
Max Parry <66670928+Buggem@users.noreply.github.com>
2025-12-05 16:05:07 +1100

    Asterix

--- pong.js
+++ pong.js
@@ -1,7 +1,7 @@
 /*
  * ATARI-Style Pong
  * Copyright (C) 2025  Buggem
-
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
